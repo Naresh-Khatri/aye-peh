a21a751369c754b7b22266278e29ee833f8034d0
Naresh Khatri <32219857+Naresh-Khatri@users.noreply.github.com>
2022-11-06 11:47:04 +0530

    use data.url to update img src

--- rand_img/script.js
+++ rand_img/script.js
@@ -5,7 +5,7 @@ function updateImage() {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      imageElement.src = data.urls.regular;
+      imageElement.src = data.url;
     });
 }
 
